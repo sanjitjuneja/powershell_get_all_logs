4e435ee606446f2168a8a1d27e4fc413fd9dd503
Sanjit Juneja <sanjit.j55@gmail.com>
2023-07-11 14:19:53 -0500

    Added split zip functionality

--- get_all_logs.ps1
+++ get_all_logs.ps1
@@ -44,11 +44,85 @@ Remove-Item -Path $tempDirectory -Recurse -Force
 $zipFileSize = (Get-Item -Path $zipFile).length
 $zipFileSizeLimit = 3GB
 
-if ($zipFileSize -gt $zipFileSizeLimit) {
-    Remove-Item -Path $zipFile
-    Write-Host "ERROR: ZIP FILE SIZE EXCEEDS LIMIT OF 3GB. PLEASE RETRIEVE FILES MANUALLY."
-    exit
-} else {
-	Write-Host "SUCCESS: ZIP FILE CREATED. TO DOWNLOAD, PLEASE RUN: 'getfile path-C:\Users\Public\Documents\logs.zip'. PLEASE DELETE FILE AFTER DOWNLOAD."
-	exit
+function Split-ZipFile {
+    param(
+        [Parameter(Mandatory=$true)][string] $file,
+        [Parameter(Mandatory=$true)][long] $size
+    )
+
+    Add-Type -TypeDefinition @"
+        public class SizeSplitStream : System.IO.Stream {
+            private string _path;
+            private long _size;
+            private int _part;
+            private System.IO.Stream _stream;
+
+            public SizeSplitStream(string path, long size) {
+                _path = path;
+                _size = size;
+                _part = 0;
+                _stream = CreateStream();
+            }
+
+            private System.IO.Stream CreateStream() {
+                string partName = GetPartName();
+                return new System.IO.FileStream(partName, System.IO.FileMode.CreateNew, System.IO.FileAccess.Write, System.IO.FileShare.None);
+            }
+
+            private string GetPartName() {
+                string directory = System.IO.Path.GetDirectoryName(_path);
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(_path);
+                string extension = System.IO.Path.GetExtension(_path);
+                return System.IO.Path.Combine(directory, string.Format("{0}_{1:D4}{2}", fileName, _part, extension));
+            }
+
+            public override bool CanRead { get { return false; } }
+            public override bool CanSeek { get { return false; } }
+            public override bool CanWrite { get { return true; } }
+            public override void Flush() { _stream.Flush(); }
+            public override long Length { get { throw new System.NotSupportedException(); } }
+            public override long Position {
+                get { throw new System.NotSupportedException(); }
+                set { throw new System.NotSupportedException(); }
+            }
+            public override int Read(byte[] buffer, int offset, int count) { throw new System.NotSupportedException(); }
+            public override long Seek(long offset, System.IO.SeekOrigin origin) { throw new System.NotSupportedException(); }
+            public override void SetLength(long value) { throw new System.NotSupportedException(); }
+
+            public override void Write(byte[] buffer, int offset, int count) {
+                if (_stream.Position + count > _size) {
+                    count = (int)(_size - _stream.Position);
+                    _stream.Write(buffer, offset, count);
+                    _stream.Dispose();
+                    _stream = CreateStream();
+                    offset += count;
+                    count = buffer.Length - offset;
+                }
+                _stream.Write(buffer, offset, count);
+            }
+
+            protected override void Dispose(bool disposing) {
+                _stream.Dispose();
+                base.Dispose(disposing);
+            }
+        }
+"@
+
+    $sourceStream = New-Object System.IO.FileStream($file, [System.IO.FileMode]::Open)
+    $destinationStream = New-Object SizeSplitStream -ArgumentList @($file, $size)
+    $sourceStream.CopyTo($destinationStream)
+    $sourceStream.Dispose()
+    $destinationStream.Dispose()
+    Remove-Item $file
 }
+
+if ($zipFileSize -gt $zipFileSizeLimit) {
+    Split-ZipFile -file $zipFile -size $zipFileSizeLimit
+    $zipFileParts = Get-ChildItem -Path $destinationDirectory -Filter "$zipFileName*"
+    foreach ($zipFilePart in $zipFileParts) {
+        Write-Host "INFO: ZIP FILE PART CREATED. TO DOWNLOAD, PLEASE RUN: 'getfile path-$($zipFilePart.FullName)'. PLEASE DELETE FILE AFTER DOWNLOADING."
+    }
+} else {
+    Write-Host "SUCCESS: ZIP FILE CREATED. TO DOWNLOAD, PLEASE RUN: 'getfile path-$zipFile'. PLEASE DELETE FILE AFTER DOWNLOADING."
+}
+exit
