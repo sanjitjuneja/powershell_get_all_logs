3225c080de2509e485351c4bb20544ed7db14874
Sanjit Juneja <sanjit.j55@gmail.com>
2023-07-11 14:46:26 -0500

    Update get_all_logs.ps1

--- get_all_logs.ps1
+++ get_all_logs.ps1
@@ -1,3 +1,9 @@
+# Ensure 7-Zip is installed
+if (!(Test-Path "${Env:ProgramFiles}\7-Zip\7z.exe")) {
+    Write-Host "7-Zip must be installed for this script to work."
+    exit
+}
+
 # SPECIFY LOGS TO PULL
 $logs = @(
     "Security",
@@ -7,7 +13,6 @@ $logs = @(
 
 # SPECIFY DESTINATION DIRECTORY
 $destinationDirectory = "C:\Users\Public\Documents"
-$zipFileName = "logs.zip"
 
 # CREATE TEMPORARY DIRECTORY
 $tempDirectory = Join-Path -Path $destinationDirectory -ChildPath "temp"
@@ -15,114 +20,59 @@ if (!(Test-Path -Path $tempDirectory)) {
     New-Item -ItemType Directory -Path $tempDirectory
 }
 
-# COPY FILES TO TEMPORARY DIRECTORY
+# EXPORT LOGS TO TEMPORARY DIRECTORY
 foreach ($log in $logs) {
-	$destinationFile = Join-Path -Path $tempDirectory -ChildPath "$log.csv"
-	Get-WinEvent -LogName $log -MaxEvents 1000 | Export-Csv -Path $destinationFile -NoTypeInformation -Force
+    $destinationFile = Join-Path -Path $tempDirectory -ChildPath "$log.evtx"
+    # Use wevtutil to export the log
+    wevtutil epl $log $destinationFile
 }
 
-# WAIT FOR ALL WRITES TO FINISH
-Start-Sleep -Seconds 15
+# Calculate total size of all log files
+$totalSize = (Get-ChildItem $tempDirectory -Recurse | Measure-Object -Property Length -Sum).Sum
 
-# CREATE ZIP FILE
+# DEFINE ZIP FILE NAME
+$zipFileName = "logs.zip"
 $zipFile = Join-Path -Path $destinationDirectory -ChildPath $zipFileName
-if (Test-Path -Path $zipFile) {
-    Remove-Item -Path $zipFile
-}
 
-try {
-    Compress-Archive -Path $tempDirectory\* -DestinationPath $zipFile -ErrorAction Stop
-} catch {
-    Write-Host "ERROR: Failed to compress the archive: $_"
-    exit
-}
+if($totalSize -gt 3GB) {
+    # If total size is greater than 3GB, use 7-Zip to split into chunks
 
-# CLEAN UP TEMPORARY DIRECTORY
-Remove-Item -Path $tempDirectory -Recurse -Force
+    # Remove existing zip file if it exists
+    if (Test-Path -Path $zipFile) {
+        Remove-Item -Path $zipFile
+    }
 
-# CHECK SIZE OF ZIP FILE (PRINT ERROR OR INITIATE DELAY)
-$zipFileSize = (Get-Item -Path $zipFile).length
-$zipFileSizeLimit = 3GB
+    try {
+        & "${Env:ProgramFiles}\7-Zip\7z.exe" a -tzip -v3g $zipFile $tempDirectory\*
+    } catch {
+        Write-Host "ERROR: Failed to compress and split the archive: $_"
+        exit
+    }
 
-function Split-ZipFile {
-    param(
-        [Parameter(Mandatory=$true)][string] $file,
-        [Parameter(Mandatory=$true)][long] $size
-    )
-
-    Add-Type -TypeDefinition @"
-        public class SizeSplitStream : System.IO.Stream {
-            private string _path;
-            private long _size;
-            private int _part;
-            private System.IO.Stream _stream;
-
-            public SizeSplitStream(string path, long size) {
-                _path = path;
-                _size = size;
-                _part = 0;
-                _stream = CreateStream();
-            }
-
-            private System.IO.Stream CreateStream() {
-                string partName = GetPartName();
-                return new System.IO.FileStream(partName, System.IO.FileMode.CreateNew, System.IO.FileAccess.Write, System.IO.FileShare.None);
-            }
-
-            private string GetPartName() {
-                string directory = System.IO.Path.GetDirectoryName(_path);
-                string fileName = System.IO.Path.GetFileNameWithoutExtension(_path);
-                string extension = System.IO.Path.GetExtension(_path);
-                return System.IO.Path.Combine(directory, string.Format("{0}_{1:D4}{2}", fileName, _part, extension));
-            }
-
-            public override bool CanRead { get { return false; } }
-            public override bool CanSeek { get { return false; } }
-            public override bool CanWrite { get { return true; } }
-            public override void Flush() { _stream.Flush(); }
-            public override long Length { get { throw new System.NotSupportedException(); } }
-            public override long Position {
-                get { throw new System.NotSupportedException(); }
-                set { throw new System.NotSupportedException(); }
-            }
-            public override int Read(byte[] buffer, int offset, int count) { throw new System.NotSupportedException(); }
-            public override long Seek(long offset, System.IO.SeekOrigin origin) { throw new System.NotSupportedException(); }
-            public override void SetLength(long value) { throw new System.NotSupportedException(); }
-
-            public override void Write(byte[] buffer, int offset, int count) {
-                if (_stream.Position + count > _size) {
-                    count = (int)(_size - _stream.Position);
-                    _stream.Write(buffer, offset, count);
-                    _stream.Dispose();
-                    _stream = CreateStream();
-                    offset += count;
-                    count = buffer.Length - offset;
-                }
-                _stream.Write(buffer, offset, count);
-            }
-
-            protected override void Dispose(bool disposing) {
-                _stream.Dispose();
-                base.Dispose(disposing);
-            }
-        }
-"@
-
-    $sourceStream = New-Object System.IO.FileStream($file, [System.IO.FileMode]::Open)
-    $destinationStream = New-Object SizeSplitStream -ArgumentList @($file, $size)
-    $sourceStream.CopyTo($destinationStream)
-    $sourceStream.Dispose()
-    $destinationStream.Dispose()
-    Remove-Item $file
-}
-
-if ($zipFileSize -gt $zipFileSizeLimit) {
-    Split-ZipFile -file $zipFile -size $zipFileSizeLimit
+    # Provide instructions for downloading zip file parts
     $zipFileParts = Get-ChildItem -Path $destinationDirectory -Filter "$zipFileName*"
     foreach ($zipFilePart in $zipFileParts) {
         Write-Host "INFO: ZIP FILE PART CREATED. TO DOWNLOAD, PLEASE RUN: 'getfile path-$($zipFilePart.FullName)'. PLEASE DELETE FILE AFTER DOWNLOADING."
     }
 } else {
-    Write-Host "SUCCESS: ZIP FILE CREATED. TO DOWNLOAD, PLEASE RUN: 'getfile path-$zipFile'. PLEASE DELETE FILE AFTER DOWNLOADING."
+    # If total size is less than 3GB, use Compress-Archive
+
+    # Remove existing zip file if it exists
+    if (Test-Path -Path $zipFile) {
+        Remove-Item -Path $zipFile
+    }
+
+    try {
+        Compress-Archive -Path $tempDirectory\* -DestinationPath $zipFile -ErrorAction Stop
+    } catch {
+        Write-Host "ERROR: Failed to compress the archive: $_"
+        exit
+    }
+
+    Write-Host "INFO: ZIP FILE CREATED. TO DOWNLOAD, PLEASE RUN: 'getfile path-$zipFile'. PLEASE DELETE FILE AFTER DOWNLOADING."
 }
+
+# CLEAN UP TEMPORARY DIRECTORY
+Remove-Item -Path $tempDirectory -Recurse -Force
+
 exit
